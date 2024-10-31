6f5a1eba240c2e5b435d32c5044996dffc285e2e
Явор Налбантов <javor.nalbantov@gmail.com>
2024-10-31 23:54:10 +0200

    updaets

--- Program.cs
+++ Program.cs
@@ -10,14 +10,14 @@ namespace ValidateDate
         {
             string[] dates = new string[]
                 {
-                    /*
+                    
                     "13/Jul/1928",
                     "10-Nov-1934",
                     "01/Jan-1951",
                     "25.Dec.1937",
                     "23#09#1973",
                     "1/Feb/2016",
-                    */
+                    
                     "01/Jan-1951",
                     "29/Feb/2024",
                     "1/Jan-1951",
